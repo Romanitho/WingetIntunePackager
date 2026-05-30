3ea5fd5a1fbee3564cfee23aecc0c278b42449df
Roman Korecky <Roman.Korecky@easyon.at>
2026-05-30 11:41:21 +0200

    Bumps Winget Intune Packager version to 1.2.3

--- sources/WingetIntunePackager.ps1
+++ sources/WingetIntunePackager.ps1
@@ -11,7 +11,7 @@ https://github.com/Romanitho/Winget-Intune-Packager
 ### APP INFO ###
 
 #Winget Intune Packager version
-$Script:WingetIntunePackager = "1.2.2"
+$Script:WingetIntunePackager = "1.2.3"
 #Winget-Install Github Link
 $Script:WIGithubLink = "https://github.com/Romanitho/Winget-Install/archive/refs/tags/v1.11.3.zip"
 #Winget Intune Packager Icon Base64
